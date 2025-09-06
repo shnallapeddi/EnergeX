5731f749bcd6d224193ae6f1d28ce37ee1e8e6cb
Sharanya Nallapeddi <nallapeddisharanya@gmail.com>
2025-09-06 19:45:55 -0400

    Update CorsMiddleware.php

--- php-api/app/Http/Middleware/CorsMiddleware.php
+++ php-api/app/Http/Middleware/CorsMiddleware.php
@@ -1,16 +1,31 @@
 <?php
+
 namespace App\Http\Middleware;
 
 use Closure;
+use Illuminate\Http\Response;
 
 class CorsMiddleware
 {
     public function handle($request, Closure $next)
     {
+        if ($request->getMethod() === 'OPTIONS') {
+            return response('', 204, [
+                'Access-Control-Allow-Origin'  => '*',
+                'Access-Control-Allow-Methods' => 'GET, POST, PUT, PATCH, DELETE, OPTIONS',
+                'Access-Control-Allow-Headers' => 'Origin, Content-Type, Accept, Authorization, X-Requested-With',
+                'Access-Control-Max-Age'       => '86400'
+            ]);
+        }
+
+        /** @var Response $response */
         $response = $next($request);
+
         $response->headers->set('Access-Control-Allow-Origin', '*');
-        $response->headers->set('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
-        $response->headers->set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+        $response->headers->set('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS');
+        $response->headers->set('Access-Control-Allow-Headers', 'Origin, Content-Type, Accept, Authorization, X-Requested-With');
+        $response->headers->set('Access-Control-Max-Age', '86400');
+
         return $response;
     }
 }
