320e58cb6b408d8b2b2d22e843afe2e45bf2557a
Sharanya Nallapeddi <nallapeddisharanya@gmail.com>
2025-09-05 20:49:16 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,16 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/EnergeX-AI/', 
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:8000',
-        changeOrigin: true,
-        secure: false,
-        rewrite: p => p,
-      },
-    },
-  },
 })
-
