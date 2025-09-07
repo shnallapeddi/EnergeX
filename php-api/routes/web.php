1b38317121a0932d323b1f6db99cf3db89ceee3d
Sharanya Nallapeddi <nallapeddisharanya@gmail.com>
2025-09-06 22:59:05 -0400

    Update web.php

--- php-api/routes/web.php
+++ php-api/routes/web.php
@@ -1,21 +1,20 @@
 <?php
 
+// Group under /api
 $router->group(['prefix' => 'api'], function () use ($router) {
-    // CORS preflight for specific endpoints
+    // Preflight for each endpoint
     $router->options('register', function () { return response('', 204); });
     $router->options('login',    function () { return response('', 204); });
     $router->options('posts',    function () { return response('', 204); });
     $router->options('posts/{id}', function () { return response('', 204); });
 
-    // Public endpoints
+    // Actual endpoints
     $router->post('register', 'AuthController@register');
     $router->post('login',    'AuthController@login');
 
-    // Protected endpoints
     $router->group(['middleware' => 'jwt.auth'], function () use ($router) {
-        $router->get('posts',       'PostController@index');
-        $router->get('posts/{id}',  'PostController@show');
-        $router->post('posts',      'PostController@store');
+        $router->get('posts',      'PostController@index');
+        $router->get('posts/{id}', 'PostController@show');
+        $router->post('posts',     'PostController@store');
     });
 });
-
