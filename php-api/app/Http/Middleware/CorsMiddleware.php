38826626348db9b11741c06dbaec0eeb651cd82b
Sharanya Nallapeddi <nallapeddisharanya@gmail.com>
2025-09-06 23:00:25 -0400

    Update CorsMiddleware.php

--- php-api/app/Http/Middleware/CorsMiddleware.php
+++ php-api/app/Http/Middleware/CorsMiddleware.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace App\Http\Middleware;
 
 use Closure;
@@ -8,12 +7,8 @@ class CorsMiddleware
 {
     public function handle($request, Closure $next)
     {
-        $origin = $request->headers->get('Origin') ?: '*';
-
         $headers = [
-            'Access-Control-Allow-Origin'      => $origin,
-            'Vary'                             => 'Origin',
-            'Access-Control-Allow-Credentials' => 'true',
+            'Access-Control-Allow-Origin'      => '*',
             'Access-Control-Allow-Methods'     => 'GET, POST, PUT, PATCH, DELETE, OPTIONS',
             'Access-Control-Allow-Headers'     => 'Content-Type, Authorization, X-Requested-With, Origin, Accept',
             'Access-Control-Max-Age'           => '86400',
@@ -24,11 +19,13 @@ class CorsMiddleware
         }
 
         $response = $next($request);
+
         foreach ($headers as $k => $v) {
             if (!$response->headers->has($k)) {
                 $response->headers->set($k, $v);
             }
         }
+
         return $response;
     }
 }
